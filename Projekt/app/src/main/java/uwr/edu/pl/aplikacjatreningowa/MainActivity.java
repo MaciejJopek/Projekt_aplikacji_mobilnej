b6770a347d190251c842d5d677012bc9abc21f3f
Maciej Jopek <297615@uwr.edu.pl>
2020-04-19 22:28:35 +0200

    Update Stopwatch button

--- Projekt/app/src/main/java/uwr/edu/pl/aplikacjatreningowa/MainActivity.java
+++ Projekt/app/src/main/java/uwr/edu/pl/aplikacjatreningowa/MainActivity.java
@@ -2,13 +2,29 @@ package uwr.edu.pl.aplikacjatreningowa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.stoper);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openStoper();
+            }
+        });
+    }
+    public void openStoper()
+    {
+        Intent intent = new Intent(this, Stoper.class);
+        startActivity(intent);
     }
 }
---
+++ Projekt/app/src/main/java/uwr/edu/pl/aplikacjatreningowa/Stoper.java
@@ -0,0 +1,16 @@
+package uwr.edu.pl.aplikacjatreningowa;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.R;
+import android.R.layout;
+
+import android.os.Bundle;
+
+public class Stoper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stoper);
+    }
+}
